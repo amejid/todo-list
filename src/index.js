1eeefb11f9a20b06199ede8ada94995ba25eae26
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-04 21:12:15 +0300

    Setup webpack with boilerplate files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'Amejid'], ' ');
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
