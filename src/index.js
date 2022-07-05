d5a8bab7c1dd51c91bed2eeeb1ea735f1d7fb25e
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-05 12:35:30 +0300

    Move functionality to CRUD js file

--- src/index.js
+++ src/index.js
@@ -1,37 +1,32 @@
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
 
 import './style.css';
 
 const listsEl = document.querySelector('.lists');
 
-const tasks = [
-  {
-    description: 'Visit friends',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Visit family',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Doctor appointment',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Finish project',
-    completed: false,
-    index: 3,
-  },
-];
+window.addEventListener('load', () => {
+  const tasks = JSON.parse(localStorage.getItem('todos')) || [];
+  if (!tasks) return;
 
-tasks.forEach((task) => {
-  const markup = `
-    <li class="list">${task.description}</>
-  `;
+  const tasksSorted = tasks.slice().sort((a, b) => a.index - b.index);
+  tasksSorted.forEach((task) => {
+    const markup = `
+      <li class="list list-${task.index}">
+        <span class="list-left">
+          <button class="btn check" data-btn="${task.index}">
+            <span class="empty-check active"><i class="fa-regular fa-square active"></i></span>
+            <span class="checked"><i class="fa-solid fa-check"></i></span>
+          </button>
+          <input type="text" class="todo" value="${task.description}"/>
+        </span>
+        <button class="btn remove" data-remove="${task.index}">
+          <i class="fa-regular fa-trash-can"></i>
+        </button>
+      </>
+    `;
 
-  listsEl.insertAdjacentHTML('beforeend', markup);
+    listsEl.insertAdjacentHTML('beforeend', markup);
+  });
 });
