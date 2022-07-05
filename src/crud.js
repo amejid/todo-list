378fd881ab442bfbd1417c72b2c79873a2d5909c
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-05 12:27:34 +0300

    Store new todo in the localStorage

--- src/crud.js
+++ src/crud.js
@@ -1,1 +1,18 @@
-const header = document.querySelector('h2');
+const formEl = document.querySelector('.input-container');
+const todoInputEl = document.querySelector('.todo-input');
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const tasks = JSON.parse(localStorage.getItem('todos')) || [];
+  const toDo = todoInputEl.value.trim();
+  if (!toDo) return;
+
+  const task = {
+    description: toDo,
+    completed: false,
+    index: tasks.length,
+  };
+
+  const tasksMod = [...tasks, task];
+  localStorage.setItem('todos', JSON.stringify(tasksMod));
+});
