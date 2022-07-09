957fd23ec2d1f0b89f9a59002dac61d1011df20b
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-10 00:07:52 +0300

    Create a bond between model and UI

--- src/index.js
+++ src/index.js
@@ -3,16 +3,49 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 
 import './style.css';
-import { clearAll, markCompleted } from './clear';
-import renderTodo from './crud';
+import displayTodosView from './views/displayTodosView';
+import * as model from './model';
+import addTodoView from './views/addTodoView';
+import removeTodoView from './views/removeTodoView';
+import editTodoView from './views/editTodoView';
+import updateStatusView from './views/updateStatusView';
+import clearTodosView from './views/clearTodosView';
 
-const listsEl = document.querySelector('.lists');
-const btnClearEl = document.querySelector('.btn-clear');
+const controlDisplayTodos = () => {
+  model.loadTodo();
+  displayTodosView.renderTodos(model.state.toDos);
+};
 
-window.addEventListener('load', renderTodo);
+const controlAddTodo = (val) => {
+  model.addTodo(val);
+  displayTodosView.renderTodos(model.state.toDos);
+};
 
-listsEl.addEventListener('click', markCompleted);
-btnClearEl.addEventListener('click', () => {
-  clearAll();
-  renderTodo();
-});
+const controlRemoveTodo = (ind) => {
+  model.removeTodo(ind);
+  displayTodosView.renderTodos(model.state.toDos);
+};
+
+const controlEditTodo = (ind, val) => {
+  model.editTodo(ind, val);
+};
+
+const controlUpdateStatus = (ind) => {
+  model.markCompleted(ind);
+};
+
+const controlClearTodos = () => {
+  model.clearAll();
+  displayTodosView.renderTodos(model.state.toDos);
+};
+
+const init = () => {
+  window.addEventListener('load', controlDisplayTodos);
+  addTodoView.addControllerAddTodo(controlAddTodo);
+  removeTodoView.addControllerRemoveTodo(controlRemoveTodo);
+  editTodoView.addControllerEditTodo(controlEditTodo);
+  updateStatusView.addControllerUpdateStatus(controlUpdateStatus);
+  clearTodosView.addControllerClearTodos(controlClearTodos);
+};
+
+init();
