e9c2e8c49b7472a62d07851dba91ee421a64efda
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-10 14:00:43 +0300

    Update config file

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/dist/',
+    publicPath: '/todo-list/',
   },
   optimization: {
     runtimeChunk: 'single',
